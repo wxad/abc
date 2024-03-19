74ff3e3f31bc36e3780db8af668be4e16841b94f
yijiejiang <yijiejiang@tencent.com>
2024-03-19 11:12:36 +0800

    chore

--- src/pages/[content]/index.tsx
+++ src/pages/[content]/index.tsx
@@ -32,9 +32,9 @@ export default function PostPage({ content }: { content: Post }) {
         <meta property="og:description" content={frontmatter.description} />
       </Head>
       <nav className="fixed 74:hidden bottom-0 p-4 left-1/2 transform -translate-x-1/2 z-20 w-[min(800px,_100vw)]">
-        <div className="flex items-center px-2 h-[48px] bg-neutral-100 bg-opacity-95 backdrop-blur-sm rounded-md shadow-md">
+        <div className="flex items-center justify-between px-4 h-[48px] bg-neutral-100 bg-opacity-95 backdrop-blur-sm rounded-md shadow-md">
           <NextLink
-            className="flex items-center gap-2 text-xs font-semibold"
+            className="flex items-center gap-2 text-xs font-semibold opacity-80"
             href="/"
           >
             <div
@@ -48,6 +48,20 @@ export default function PostPage({ content }: { content: Post }) {
             />
             设计垂点
           </NextLink>
+          <a
+            className="flex"
+            href="https://github.com/wxad/abc"
+            target="_blank"
+          >
+            <svg width="24" height="24" viewBox="0 0 32 32" fill="none">
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M16 0C7.16 0 0 7.16 0 16C0 23.08 4.58 29.06 10.94 31.18C11.74 31.32 12.04 30.84 12.04 30.42C12.04 30.04 12.02 28.78 12.02 27.44C8 28.18 6.96 26.46 6.64 25.56C6.46 25.1 5.68 23.68 5 23.3C4.44 23 3.64 22.26 4.98 22.24C6.24 22.22 7.14 23.4 7.44 23.88C8.88 26.3 11.18 25.62 12.1 25.2C12.24 24.16 12.66 23.46 13.12 23.06C9.56 22.66 5.84 21.28 5.84 15.16C5.84 13.42 6.46 11.98 7.48 10.86C7.32 10.46 6.76 8.82 7.64 6.62C7.64 6.62 8.98 6.2 12.04 8.26C13.32 7.9 14.68 7.72 16.04 7.72C17.4 7.72 18.76 7.9 20.04 8.26C23.1 6.18 24.44 6.62 24.44 6.62C25.32 8.82 24.76 10.46 24.6 10.86C25.62 11.98 26.24 13.4 26.24 15.16C26.24 21.3 22.5 22.66 18.94 23.06C19.52 23.56 20.02 24.52 20.02 26.02C20.02 28.16 20 29.88 20 30.42C20 30.84 20.3 31.34 21.1 31.18C24.2763 30.1077 27.0363 28.0664 28.9917 25.3432C30.947 22.6201 31.9991 19.3524 32 16C32 7.16 24.84 0 16 0Z"
+                fill="rgba(0, 0, 0, 0.6)"
+              ></path>
+            </svg>
+          </a>
         </div>
       </nav>
       <nav className="fixed hidden 74:flex flex-col justify-between top-16 bottom-20 pl-6 max-w-44 text-gray-600">
