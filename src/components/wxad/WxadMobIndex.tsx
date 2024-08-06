2f00c7baed5276094e60ec9a5ba7bf436f46ba59
yijiejiang <yijiejiang@tencent.com>
2024-08-06 11:55:19 +0800

    chore: update

--- src/components/wxad/WxadMobIndex.tsx
+++ src/components/wxad/WxadMobIndex.tsx
@@ -5,7 +5,7 @@ const Demo = () => {
     <DemoBox className="flex flex-col items-center py-8 overflow-hidden">
       <img
         className="w-[240px] max-w-full"
-        src="https://wxa.wxs.qq.com/wxad-design/yijie/wxad-motion-basic.gif"
+        src="https://wxa.wxs.qq.com/wxad-design/yijie/wxad-con.gif"
         loading="lazy"
         style={{
           clipPath: "inset(1px round 12px)",
