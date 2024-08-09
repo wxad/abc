f1d0b63fc3a117c11e8964c0b76644e400e415e3
yijiejiang <yijiejiang@tencent.com>
2024-08-09 14:52:21 +0800

    chore: fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,6 +13,18 @@ export default function Document() {
               "url(https://wxa.wxs.qq.com/wxad-design/yijie/noise.png)",
           }}
         />
+        <div
+          style={{
+            position: "fixed",
+            top: "-9999px",
+            left: "-9999px",
+          }}
+        >
+          防止微信内出现全文翻译提示。因此在这里放一个充满中文的分区。
+          防止微信内出现全文翻译提示。因此在这里放一个充满中文的分区。
+          防止微信内出现全文翻译提示。因此在这里放一个充满中文的分区。
+          防止微信内出现全文翻译提示。因此在这里放一个充满中文的分区。
+        </div>
         <Main></Main>
         <NextScript />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/css-doodle/0.39.0/css-doodle.min.js"></script>
