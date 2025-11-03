ca9f03fe466ed8af6bb57b0bbe0e642ecc42095a
yijiejiang <yijiejiang@tencent.com>
2025-11-03 20:33:13 +0800

    chore

--- next.config.mjs
+++ next.config.mjs
@@ -4,12 +4,14 @@ const nextConfig = {
   basePath: "/abc",
   reactStrictMode: false,
   reactCompiler: true,
-  forceSwcTransforms: true,
-  largePageDataBytes: 128 * 100000,
-  cacheComponents: true,
   // swcMinify: false,
   images: {
-    domains: ["avatars.githubusercontent.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
   },
   experimental: {
     turbopackFileSystemCacheForDev: true,
